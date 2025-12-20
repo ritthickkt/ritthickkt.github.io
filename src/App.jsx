fb9e0af4ae01119acafa71320f713dab5589c873
Ritthick Thiaga <ritthickthiaga@gmail.com>
2025-12-20 22:07:30 +1100

    removed horizontal scroll for projects

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,12 @@ import projects from '/src/assets/projects.png'
 import settings from '/src/assets/settings.png'
 import Lenis from 'lenis'
 import { useEffect} from 'react'
-// import achievements from 'src/assets/achievements.png'
 import work from '/src/assets/work.png' 
 import Projects from '/src/components/Projects.jsx'
-import Projects2 from '/src/components/Projects2.jsx'
-import MobileProjects from '/src/components/MobileProjects.jsx'
-import MobileProjects2 from '/src/components/MobileProjects2.jsx'
-import MobileProjects3 from '/src/components/MobileProjects3.jsx'
 import WorkExperience from './components/WorkExperience'
 import LetsConnect from './components/LetsConnect.jsx'
 
 function App() {
-  // useEffect(() => {
-  //   const handleResize = () => setIsMobile(window.innerWidth <= 800);
-  //   window.addEventListener('resize', handleResize);
-  //   return () => window.removeEventListener('resize', handleResize);
-  // }, []);
 
   useEffect(() => {
     const lenis = new Lenis();
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,11 +19,10 @@ import supabase from "/src/assets/supabase.svg"
 function Projects() {
   return (
     <>
-    <span className='scroll-indicator'>Scroll →</span>
     <div className="project-stack">
       <div className='projects'>
-        <Project projectTitle="Airtable Clone" projectDescription="Built with Supabase, Prisma, and T3 stack, made to replicate the original Airtable website application with an additional button to add 100k rows." projectImage={airtable} projectStack={[react,nextjs,supabase]}/>
-        <Project id="firstgal" projectTitle="Code Snippet" projectDescription="A public VS Code Extension that uses Paste Bin API to create sharable links for code snippets" projectLink="https://github.com/ritthickkt/codeSnippet" projectImage={codesnippet} projectStack={[javascript]} />
+        <Project projectTitle="Airtable Clone" projectDescription="Built with Supabase, Prisma, and T3 stack, made to replicate the original Airtable website application with an additional button to add 100k rows." projectLink="https://github.com/ritthickkt/airtable_clone" projectImage={airtable} projectStack={[react,nextjs,supabase]}/>
+        <Project projectTitle="Code Snippet" projectDescription="A public VS Code Extension that uses Paste Bin API to create sharable links for code snippets" projectLink="https://github.com/ritthickkt/codeSnippet" projectImage={codesnippet} projectStack={[javascript]} />
         <Project projectTitle="Weather App" projectDescription="Built with Open Meteo API and motion providing current temperature, hourly forecasts, and weekly forecasts." projectLink="https://github.com/ritthickkt/weather-app" projectImage={weatherapp} projectStack={[react,vite]}/>
         <Project projectTitle="Pomodoro Timer" projectDescription="Built with Javascript and Electron, providing a 25 and 50 minute pomodoro with 5 and 10 minute breaks respectively" projectLink="https://github.com/ritthickkt/pomodoro-timer" projectImage={pomodoro} projectStack={[javascript, electron]}/>
         <Project projectTitle="Chat App" projectDescription="Group chat application built with firebase, react and electron. Integrates a SSO Login page with Google. " projectLink="https://github.com/ritthickkt/chat-app" projectImage={chatapp} projectStack={[firebase, electron, vite, react]}/>
